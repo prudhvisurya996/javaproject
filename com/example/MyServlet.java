19597f78d30b4e90741ff658808ac99f317401e2
prudhvisurya996 <prudhvisurya996@gmail.com>
2024-12-15 11:10:29 +0530

    samplejavaproject

---
+++ com/example/MyServlet.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MyServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from MyServlet!</h1>");
+    }
+}
